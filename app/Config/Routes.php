f8f2f2561cb7b914f595c141841a1612b220e942
roroyyan <royyanfirdaus78@gmail.com>
2025-07-03 20:58:41 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,13 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
 $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('contact', 'ContactController::index', ['filter' => 'auth']);
+
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -36,6 +36,14 @@
             <!-- End Produk Nav -->
 
             <li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+             <i class="bi bi-person"></i>
+                 <span>Profile</span>
+             </a>
+            </li>
+            <!-- End Profile Nav -->
+            
+            <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'contact') ? "" : "collapsed" ?>" href="contact">
                     <i class="bi bi-envelope"></i>
                     <span>Contact</span>
